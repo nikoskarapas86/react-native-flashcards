9ddae4faf317bcb00bd94a0d7fa38edd24227186
nikolaos karapas <nikos.karapas86@gmail.com>
2021-05-05 17:33:14 +0300

    bug fixed

--- components/Quiz.js
+++ components/Quiz.js
@@ -14,7 +14,7 @@ class Quiz extends Component {
   state = {
     currentQuestion: 0,
     correctAnswers: 0,
-    faceUp: true,
+    showQuest: true,
   };
 
   handleCorrect = () => {
@@ -30,48 +30,73 @@ class Quiz extends Component {
     }));
   };
 
+  resetQuiz = () => {
+    this.setState(() => ({
+      currentQuestion: 0,
+      correctAnswers: 0,
+      showQuest: true,
+    }));
+  };
+
   render() {
     const { deck } = this.props;
     const { currentQuestion } = this.state;
     const questLength = deck.questions.length;
     const { question, answer } =
-      questLength !== undefined && questLength > 0 && (currentQuestion <= questLength)
+      questLength !== undefined &&
+      questLength > 0 &&
+      currentQuestion < questLength
         ? deck.questions[currentQuestion]
         : { question: null, answer: null };
     console.log(question, answer);
 
     return !!questLength ? (
-      <ScrollView>
-        <View style={styles.root}>
-          <Text>{`${currentQuestion + 1} / ${questLength}`}</Text>
-          <Text>{this.state.faceUp ? question : answer}</Text>
+      currentQuestion >= questLength ? (
+        <ScrollView>
+          <Text>results</Text>
+          <View style={styles.root}>
+            <Text>
+              {`results of quiz ${(
+                (this.state.correctAnswers / questLength) *
+                100
+              ).toFixed(2)}%`}
+            </Text>
+          </View>
+          <View style={styles.root}>
+            <TouchableHighlight onPress={this.resetQuiz}>
+              <Text>RESTART QUIZ</Text>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={() =>  this.props.navigation.goBack()}>
+              <Text>GO BACK</Text>
+            </TouchableHighlight>
+          </View>
+        </ScrollView>
+      ) : (
+        <ScrollView>
+          <View style={styles.root}>
+            <Text>{`${currentQuestion + 1} / ${questLength}`}</Text>
+            <Text>{this.state.showQuest && question ? question : answer}</Text>
 
-          <TextButton
-            onPress={() =>
-              this.setState((prevState) => ({ faceUp: !prevState.faceUp }))
-            }
-          >
-            Show answer or question
-          </TextButton>
-        </View>
-        <View style={styles.root}> 
-          <TouchableHighlight onPress={this.handleCorrect}>
-            <Text>CORRECT</Text>
-          </TouchableHighlight>
-          <TouchableHighlight onPress={this.handleIncorrect}>
-            <Text>INCORRECT</Text>
-          </TouchableHighlight>
-        </View>
-        <View style={styles.root}>
-        <Text>
-          {`results of quiz ${(
-            (this.state.correctAnswers / questLength) *
-            100
-          ).toFixed(2)}%`}
-        </Text> 
-        </View>
-     
-      </ScrollView>
+            <TextButton
+              onPress={() =>
+                this.setState((prevState) => ({
+                  showQuest: !prevState.showQuest,
+                }))
+              }
+            >
+              Show answer or question
+            </TextButton>
+          </View>
+          <View style={styles.root}>
+            <TouchableHighlight onPress={this.handleCorrect}>
+              <Text>CORRECT</Text>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={this.handleIncorrect}>
+              <Text>INCORRECT</Text>
+            </TouchableHighlight>
+          </View>
+        </ScrollView>
+      )
     ) : (
       <View style={styles.root}>
         <ScrollView>
